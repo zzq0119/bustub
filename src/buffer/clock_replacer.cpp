b31f4d535c020f7a6369799a58f9e228f4377830
zzq <zzqalpha@qq.com>
2021-05-09 08:22:44 -0700

    add clock replacer

--- src/buffer/clock_replacer.cpp
+++ src/buffer/clock_replacer.cpp
@@ -14,16 +14,42 @@
 
 namespace bustub {
 
-ClockReplacer::ClockReplacer(size_t num_pages) {}
+ClockReplacer::ClockReplacer(size_t num_pages) : num_pages_(num_pages) {}
 
 ClockReplacer::~ClockReplacer() = default;
 
-bool ClockReplacer::Victim(frame_id_t *frame_id) { return false; }
+bool ClockReplacer::Victim(frame_id_t *frame_id) {
+  std::lock_guard<std::mutex> guard(latch);
+  if (frame_map_.empty()) {
+    return false;
+  } else {
+    ReplaceFrame();
+    *frame_id = frame_list_.front().first;
+    EraseFrame(*frame_id);
+    return true;
+  }
+}
 
-void ClockReplacer::Pin(frame_id_t frame_id) {}
+void ClockReplacer::Pin(frame_id_t frame_id) {
+  std::lock_guard<std::mutex> guard(latch);
+  if (frame_map_.find(frame_id) != frame_map_.end()) {
+    EraseFrame(frame_id);
+  }
+}
 
-void ClockReplacer::Unpin(frame_id_t frame_id) {}
+void ClockReplacer::Unpin(frame_id_t frame_id) {
+  std::lock_guard<std::mutex> guard(latch);
+  if (frame_map_.find(frame_id) != frame_map_.end()) {
+    frame_map_.at(frame_id)->second = true;
+  } else {
+    ReplaceFrame();
+    AddFrame(frame_id);
+  }
+}
 
-size_t ClockReplacer::Size() { return 0; }
+size_t ClockReplacer::Size() {
+  std::lock_guard<std::mutex> guard(latch);
+  return frame_list_.size();
+}
 
 }  // namespace bustub
--- src/include/buffer/clock_replacer.h
+++ src/include/buffer/clock_replacer.h
@@ -14,6 +14,7 @@
 
 #include <list>
 #include <mutex>  // NOLINT
+#include <unordered_map>
 #include <vector>
 
 #include "buffer/replacer.h"
@@ -47,6 +48,36 @@ class ClockReplacer : public Replacer {
 
  private:
   // TODO(student): implement me!
+  std::list<std::pair<frame_id_t, bool>> frame_list_;
+
+  using Iter = std::list<std::pair<frame_id_t, bool>>::iterator;
+  std::unordered_map<frame_id_t, Iter> frame_map_;
+
+  size_t num_pages_;
+
+  mutable std::mutex latch;
+
+  inline void AddFrame(frame_id_t frame_id) {
+    frame_list_.emplace_back(frame_id, false);
+    frame_map_.insert({frame_id, --frame_list_.end()});
+  }
+
+  inline void EraseFrame(frame_id_t frame_id) {
+    frame_list_.erase(frame_map_.at(frame_id));
+    frame_map_.erase(frame_id);
+  }
+
+  inline void ReplaceFrame() {
+    while (frame_map_.size() == num_pages_) {
+      if (auto p = frame_list_.front(); p.second) {
+        frame_list_.pop_front();
+        p.second = false;
+        frame_list_.emplace_back(p);
+      } else {
+        EraseFrame(p.first);
+      }
+    }
+  }
 };
 
 }  // namespace bustub
