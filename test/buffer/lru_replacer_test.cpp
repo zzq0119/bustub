2521239efc9bf5ece3059fb9bef0f8f0af9db241
zzq <498328710@qq.com>
2021-08-09 10:05:15 +0800

    Revert "fix buffer pool bugs"
    This reverts commit 3585d19a80c51195d44471beb3ce71a16aee9a1c.

--- test/buffer/lru_replacer_test.cpp
+++ test/buffer/lru_replacer_test.cpp
@@ -35,81 +35,81 @@ TEST(LRUReplacerTest, SampleTest) {
   // Scenario: get three victims from the lru.
   int value;
   lru_replacer.Victim(&value);
+  EXPECT_EQ(1, value);
+  lru_replacer.Victim(&value);
   EXPECT_EQ(2, value);
   lru_replacer.Victim(&value);
   EXPECT_EQ(3, value);
-  lru_replacer.Victim(&value);
-  EXPECT_EQ(4, value);
 
   // Scenario: pin elements in the replacer.
-  // Note that 4 has already been victimized, so pinning 4 should have no effect.
+  // Note that 3 has already been victimized, so pinning 3 should have no effect.
+  lru_replacer.Pin(3);
   lru_replacer.Pin(4);
-  lru_replacer.Pin(5);
   EXPECT_EQ(2, lru_replacer.Size());
 
-  // Scenario: unpin 5. We expect that the reference bit of 5 will be set to 1.
-  lru_replacer.Unpin(5);
+  // Scenario: unpin 4. We expect that the reference bit of 4 will be set to 1.
+  lru_replacer.Unpin(4);
 
   // Scenario: continue looking for victims. We expect these victims.
   lru_replacer.Victim(&value);
+  EXPECT_EQ(5, value);
+  lru_replacer.Victim(&value);
   EXPECT_EQ(6, value);
   lru_replacer.Victim(&value);
-  EXPECT_EQ(1, value);
-  lru_replacer.Victim(&value);
-  EXPECT_EQ(5, value);
+  EXPECT_EQ(4, value);
 }
 
-TEST(LRUReplacerTest, SampleTest1) {
-  LRUReplacer lru_replacer(3);
-  frame_id_t value;
+// TEST(LRUReplacerTest, SampleTest1) {
+//   LRUReplacer lru_replacer(3);
+//   frame_id_t value;
 
-  EXPECT_EQ(false, lru_replacer.Victim(&value));
+//   EXPECT_EQ(false, lru_replacer.Victim(&value));
 
-  lru_replacer.Unpin(0);
-  EXPECT_EQ(1, lru_replacer.Size());
-  EXPECT_EQ(true, lru_replacer.Victim(&value));
-  EXPECT_EQ(0, value);
-  EXPECT_EQ(false, lru_replacer.Victim(&value));
+//   lru_replacer.Unpin(0);
+//   EXPECT_EQ(1, lru_replacer.Size());
+//   EXPECT_EQ(true, lru_replacer.Victim(&value));
+//   EXPECT_EQ(0, value);
+//   EXPECT_EQ(false, lru_replacer.Victim(&value));
 
-  lru_replacer.Pin(0);
-  EXPECT_EQ(0, lru_replacer.Size());
+//   lru_replacer.Pin(0);
+//   EXPECT_EQ(0, lru_replacer.Size());
 
-  lru_replacer.Unpin(1);
-  lru_replacer.Unpin(1);
-  lru_replacer.Unpin(2);
-  lru_replacer.Unpin(2);
-  lru_replacer.Unpin(1);
-  EXPECT_EQ(2, lru_replacer.Size());
-  EXPECT_EQ(true, lru_replacer.Victim(&value));
-  EXPECT_EQ(2, value);
-}
+//   lru_replacer.Unpin(1);
+//   lru_replacer.Unpin(1);
+//   lru_replacer.Unpin(2);
+//   lru_replacer.Unpin(2);
+//   lru_replacer.Unpin(1);
+//   EXPECT_EQ(2, lru_replacer.Size());
+//   EXPECT_EQ(true, lru_replacer.Victim(&value));
+//   EXPECT_EQ(2, value);
+// }
 
-TEST(LRUReplacerTest, BasicTest) {
-  LRUReplacer lru_replacer(100);
+// TEST(LRUReplacerTest, BasicTest) {
+//   LRUReplacer lru_replacer(100);
 
-  // push element into replacer
-  for (int i = 0; i < 100; ++i) {
-    lru_replacer.Unpin(i);
-  }
-  EXPECT_EQ(100, lru_replacer.Size());
+//   // push element into replacer
+//   for (int i = 0; i < 100; ++i) {
+//     lru_replacer.Unpin(i);
+//   }
+//   EXPECT_EQ(100, lru_replacer.Size());
 
-  // reverse then insert again
-  for (int i = 0; i < 100; ++i) {
-    lru_replacer.Unpin(99 - i);
-  }
+//   // reverse then insert again
+//   for (int i = 0; i < 100; ++i) {
+//     lru_replacer.Unpin(99 - i);
+//   }
 
-  // erase 50 element from the tail
-  for (int i = 0; i < 50; ++i) {
-    lru_replacer.Pin(i);
-  }
+//   // erase 50 element from the tail
+//   for (int i = 0; i < 50; ++i) {
+//     lru_replacer.Pin(i);
+//   }
 
-  // check left
-  frame_id_t value = -1;
-  for (int i = 99; i >= 50; --i) {
-    lru_replacer.Victim(&value);
-    EXPECT_EQ(i, value);
-    value = -1;
-  }
-}
+//   // check left
+//   frame_id_t value = -1;
+//   for (int i = 99; i >= 50; --i) {
+//     lru_replacer.Victim(&value);
+//     EXPECT_EQ(i, value);
+//     value = -1;
+//   }
+// }
 
 }  // namespace bustub
